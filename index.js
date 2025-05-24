783173169be1717e560c68400cc48baef3a68f91
yannhallage <contact.devhllg@gmail.com>
2025-05-24 01:10:22 +0000

    simple type

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-var variable_name = "2001";
-console.log(variable_name);
+// simple types 
+// number , boolean , string 
+var nombre = 2000;
+var edad = 30;
+var nombreCompleto = "Juan Carlos";
+var edadCompleta = "30 años";
+var nombreCompleto2 = "Mi nombre es ".concat(nombre, " y tengo ").concat(edad);
+var Myname = 20001;
+var myname_second;
+// boolean 
+var estaVacio;
+estaVacio = true;
+myname_second = "yann hallage " + 2000 + true;
+console.log(myname_second);
+Myname = 20001;
+// showing 
+console.log(nombreCompleto2 + " and booleans : " + estaVacio);
--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
-let variable_name: string = "2001";
-console.log(variable_name);
+// simple types 
+// number , boolean , string 
+
+let nombre : number = 2000; 
+let edad : number = 30;
+let nombreCompleto : string = "Juan Carlos";
+let edadCompleta : string = "30 años";
+let nombreCompleto2 : string = `Mi nombre es ${nombre} y tengo ${edad}`
+let Myname = 20001;
+let myname_second: any ; 
+// boolean 
+let estaVacio : boolean;
+estaVacio = true;
+
+myname_second = "yann hallage " + 2000 + true
+console.log(myname_second);
+Myname = 20001
+// showing 
+
+console.log(nombreCompleto2 + " and booleans : " + estaVacio)
