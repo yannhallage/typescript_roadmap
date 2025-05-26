a208d480acddde2e1cc9efedaf2480713eea2d99
yannhallage <contact.devhllg@gmail.com>
2025-05-26 01:29:25 +0000

    juste un simple programme

---
+++ TS program/program.ts
@@ -0,0 +1,11 @@
+
+
+// just un simple programme avec ts 
+// okay donc ici je vais essayer de creer un code permettant de 
+// resoudre un probleme 
+
+{
+    const executer = () =>{console.log(true)}
+}
+
+executer();
